58c7c2b5d25c6c80c48c91db20f6e7e18155c01b
tongjong <147476187+tongjong@users.noreply.github.com>
2023-11-24 15:02:26 +0800

    Design Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"',
+        lg: '"nav nav" "aside main"',
+      }}
+    >
+      <GridItem area="nav" bg="orange">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
