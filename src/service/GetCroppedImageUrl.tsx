0da9b2a9b9eee1a87addc8213d507cc4b2e0ee1a
Tong Jong <tongmi.jong@gmail.com>
2023-11-30 15:31:00 +0800

    Handle games with no image

--- src/service/GetCroppedImageUrl.tsx
+++ src/service/GetCroppedImageUrl.tsx
@@ -1,5 +1,7 @@
+import images from "../assets/images.jpg";
+
 const GetCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return images;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
